93ff82a298e4e53285eee8c823796b8071f56ac2
yonigozlan <yoni.gozlan@student-cs.fr>
2024-03-06 12:21:08 -0800

    sherlock

--- CLIFF/eval.py
+++ CLIFF/eval.py
@@ -526,6 +526,6 @@ def eval_dataset(root_dir, annotation_path):
 
 
 if __name__ == "__main__":
-    eval_dataset("../../", "combined_dataset_15fps/test/annotations.json")
-    # eval_dataset("/scratch/users/yonigoz/RICH/downsampled/", "val_annotations.json")
+    # eval_dataset("../../", "combined_dataset_15fps/test/annotations.json")
+    eval_dataset("/scratch/users/yonigoz/RICH/downsampled/", "val_annotations.json")
     # eval_dataset("/scratch/users/yonigoz/BEDLAM/data/", "val_annotations.json")
